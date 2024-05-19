24de4de1ec1739880f590bdf571f1e0074891dd2
Bilal Tariq <bilza2023@gmail.com>
2024-05-19 17:45:10 +0500

    ui:0.3 uploaded next 0.4

--- src/lib/appComp/examplesCard/ExamplesCard.svelte
+++ src/lib/appComp/examplesCard/ExamplesCard.svelte
@@ -6,11 +6,11 @@ import MathCard from '$lib/homePage/MathCard.svelte';
 
 
 <div class='flex justify-center flex-wrap gap-4 '>
-    <a href="https://taleem.help/player?tcode=fbise9mathOld&filename=fbise9math_ch_10_ex_10.1_q_1_n_What_is_a_Postulate" target="_blank"><MathCard  name='What is Postulate'  /></a>
+    <a href="https://taleem.help/player?tcode=fbise9mathOld&filename=fbise9math_ch_10_ex_10.1_q_1_n_What_is_a_Postulate" target="_blank"><MathCard  name='What is Postulate?'  /></a>
       
 
-    <a href="/player?tcode=fbise9math&id=659e880792faba116b079d96" target="_blank">
-        <MathCard  name='Blank'  />  
+    <a href="https://taleem.help/player?tcode=fbise9mathOld&filename=fbise_cl_9_ch_1_ex_1.1_q_2_pt_0" target="_blank">
+        <MathCard  name='Cl 9 Ex 1.1 Q 2-fb'  />  
     </a>
     
     <a href="/player?tcode=fbise9math&id=659e881192faba116b079ecd" target="_blank">
